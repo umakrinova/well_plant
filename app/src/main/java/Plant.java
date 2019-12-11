b1778c81bb84b4048c401cde62f420473c8adc25
DESKTOP-22J8Q8G\Darya <lildeadwalrus@gmail.com>
2019-12-11 21:09:32 +0300

    Plant class is here!!!

---
+++ app/src/main/java/Plant.java
@@ -0,0 +1,25 @@
+public class Plant {
+    public final String name;
+    private final int imageResource;
+    private final String imageUrl;
+
+    public Plant(String name, int imageResource, String imageUrl) {
+        this.name = name;
+        this.imageResource = imageResource;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+
+}
